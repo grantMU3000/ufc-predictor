e45b0cbc96dfac49eec00b05b96aa01bd9db24b7
Grant <robinsonjg64@gmail.com>
2026-08-04 17:00:55 -0400

    Fixed CI test failures

--- migrations/env.py
+++ migrations/env.py
@@ -1,10 +1,9 @@
 import os
 from logging.config import fileConfig
 
-from dotenv import load_dotenv
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
 from alembic import context
+from dotenv import load_dotenv
+from sqlalchemy import engine_from_config, pool
 
 load_dotenv()
 
--- migrations/versions/38e3db2c80a5_create_initial_schema.py
+++ migrations/versions/38e3db2c80a5_create_initial_schema.py
@@ -5,17 +5,17 @@ Revises:
 Create Date: 2026-08-04 14:43:00.121102
 
 """
-from typing import Sequence, Union
-from sqlalchemy.dialects import postgresql
-from alembic import op
-import sqlalchemy as sa
+from collections.abc import Sequence
 
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision: str = '38e3db2c80a5'
-down_revision: Union[str, Sequence[str], None] = None
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | Sequence[str] | None = None
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
